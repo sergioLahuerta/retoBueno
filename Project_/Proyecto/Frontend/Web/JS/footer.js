790c4503f4b44499c0208f98d4c8333fbfe3fc56
Alexri24 <a28312@svalero.com>
2025-05-27 21:00:10 +0200

    Delete Project_ directory

--- Project_/Proyecto/Frontend/Web/JS/footer.js
+++
@@ -1,9 +0,0 @@
-  window.addEventListener('DOMContentLoaded', () => {
-    const hash = window.location.hash;
-    if (hash) {
-      const checkbox = document.querySelector(hash);
-      if (checkbox && checkbox.type === 'checkbox') {
-        checkbox.checked = true;
-      }
-    }
-  });
