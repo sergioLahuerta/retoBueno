f21722b5413c573fcb7674c7fee3bb4247e68e99
Alexri24 <a28312@svalero.com>
2025-05-27 20:23:05 +0200

    Delete Project_ directory

--- Project_/Proyecto/Frontend/Web/JS/TEMPNoJsSimulator.js
+++
@@ -1,8 +0,0 @@
-  (function () {
-    // Tu código aquí queda aislado del scope global
-    const btn = document.getElementById('mi-boton');
-    btn?.addEventListener('click', (e) => {
-      e.preventDefault();
-      console.log('Click controlado sin refresh');
-    });
-  })();
--- Project_/Proyecto/Frontend/Web/sampleApi/api.js
+++
@@ -1,5 +0,0 @@
-async function getItems() {
-    const response = await fetch('/sampleApi/mockup/items.json'); // <- Espera la respuesta
-    const items = await response.json();                 // <- Espera el JSON
-    return items;
-}
